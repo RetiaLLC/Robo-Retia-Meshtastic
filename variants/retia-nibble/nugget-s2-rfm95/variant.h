9e23022fcc081e38da13cdf4d97ccfab5a65e25b
Skickar <40251293+skickar@users.noreply.github.com>
2026-04-01 17:10:24 -0700

    update fgor v6 backpacks

--- variants/retia-nibble/nugget-s2-rfm95/variant.h
+++ variants/retia-nibble/nugget-s2-rfm95/variant.h
@@ -3,10 +3,11 @@
 
 #define USE_SH1106
 
-#define LED_PIN 15 // If defined we will blink this LED
+#define LED_POWER 15 // Updated to modern Meshtastic heartbeat macro
+#define LED_STATE_ON 1 // Tells Meshtastic the LED circuit is Active-High
 
 #define HAS_NEOPIXEL                         // Enable the use of neopixels
-#define NEOPIXEL_COUNT 2                     // How many neopixels are connected
+#define NEOPIXEL_COUNT 1                     // Fixed: v6 Schematic shows 1 WS2812B chip
 #define NEOPIXEL_DATA 12                     // gpio pin used to send data to the neopixels
 #define NEOPIXEL_TYPE (NEO_GRB + NEO_KHZ800) // type of neopixels in use
 
@@ -16,10 +17,10 @@
 #define BUTTON_NEED_PULLUP
 
 #define USE_RF95
-#define LORA_SCK 6
-#define LORA_MISO 8
-#define LORA_MOSI 10
-#define LORA_CS 13
+#define LORA_SCK 4   // Fixed for v6.0
+#define LORA_MISO 6  // Fixed for v6.0
+#define LORA_MOSI 8  // Fixed for v6.0
+#define LORA_CS 10   // Fixed for v6.0
 #define LORA_DIO0 16
 #define LORA_RESET 5
 
